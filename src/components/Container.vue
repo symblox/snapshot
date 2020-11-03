0f50333fc17d343d40f1bae89e6e2f23c9ca6d2b
Symblox Dev <symblox@protonmail.com>
2020-11-03 11:56:46 +0800

    Update header style

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="slim ? 'px-0 px-md-4' : 'px-4'" class="container-lg mx-auto">
+  <div :class="slim ? 'px-0 px-md-4' : 'px-3'" class="container-lg mx-auto">
     <slot />
   </div>
 </template>
